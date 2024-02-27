c8e396cacae0a629762c970fb20d02e3924064d3
Xiaoqi Lin <58313795+banterlin@users.noreply.github.com>
2024-02-27 14:22:21 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,42 @@
+<img src="logo.png" width="200" height="200" align=right />
+
+This repository provides the code for [Deep Learning Decision Support Recommends Therapeutic Planning in Unresectable-Hepatocellular Carcinoma]. Based on the code, you can easily train your own SELECTION by configuring your own dataset and modifying the training details (such as optimizer, learning rate, etc).
+
+## Overview
+SELECTION is a ransformer-based multi-modal medical prognosis model. It differs from the traditional multi-modal apporach of fusing features from CNN with clinical data to classify clinical endpoint, where SELECTION considers holistic multi-modal information from CECT images as well as clinical information as sequences of tokens.
+
+## Setup the Environment
+This software was implemented a system running `Windows 10`, with `Python 3.9`, `PyTorch 2.0.1`, and `CUDA 11.7.1`.
+
+You can adjust the batch size to adapt to your own hardware environment. Personally, we recommend the use of four NVIDIA GPUs.
+
+## Code Description
+The main architecture of SELECTION lies in the `models/` folder. The `modeling_selection.py` is the main backbone, while the rest necessary modules are distributed into different files based on their own functions, i.e., `attention.py`, `block.py`, `configs.py`, `embed.py`, `encoder.py`, and `mlp.py`. Please refer to each file to acquire more implementation details. 
+
+**Parameter description**:
+
+`--CLS`: number of classification (binary in this study)
+
+`--BSZ`: batch size.
+
+`--DATA_DIR`: Folder path for the imaging data. arranged to have portal and arterial subfolers inside.
+
+`--SET_TYPE`: file name of the clinical baseline data (`***.pkl`).
+
+Note that `xxx.pkl` is a dictionary that stores the clinical textual data in the format of `key-value`. Here is a short piece of code showing how to organize the `***.pkl`:
+```python
+>>> import pickle
+>>> f = open('***.pkl', 'rb')
+>>> subset = pickle.load(f)
+>>> f.close()
+>>> list(subset.keys())[0:10] # display top 10 case ids
+>>> key = list(subset.keys())[0] # select the 1st key
+>>> subset[key] # display the clinical data
+>>> subset[key]['pdesc'] # the chief complaint feature
+>>> subset[key]['bics'] # the demographics information (age and sex)
+>>> subset[key]['bts'] #  the laboratory test results
+>>> subset[key]['label'] # the disease labels
+```
+
+```The code used in this studied were adopted from [A transformer-based representation-learning model with unified processing of multimodal input for clinical diagnostics,doi: 10.1038/s41551-023-01045-x]
+```
---
+++ SELECTION.py
@@ -0,0 +1,206 @@
+from __future__ import print_function, division
+import os
+import sys
+import torch
+import pandas as pd
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
+from torch import nn
+import pickle
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score,roc_curve
+from models.modeling_selection import SELECTION, CONFIGS
+from tqdm import tqdm
+from sklearn.metrics import confusion_matrix
+
+from lifelines.utils import concordance_index
+
+import argparse
+import warnings
+if not sys.warnoptions:
+    warnings.simplefilter("ignore")
+
+
+tk_lim = 40
+
+disease_list = ['OS','Non-OS']
+
+parser = argparse.ArgumentParser(description="Trainning SELECTION for HAIC/TACE Treatment Decision")
+parser.add_argument('--CLS', action='store', dest='CLS', type=int,default=2,help='No of classes')
+parser.add_argument('--BSZ', action='store', dest='BSZ', type=int,default=8,help='Batch Size')
+parser.add_argument('--epochs', action='store', dest='epochs', type=int,default=150,help='Epoch Size')
+parser.add_argument('--lr', action='store', dest='lr', type=int,default=3e-5,help='Learning Rate')
+parser.add_argument('--seed', action='store', dest='seed', type=int,default=555,help='Trainning seed')
+parser.add_argument('--EXP', action='store', dest='EXP', type=str,default='cHAIC',help='TACE/HAIC/TC/HC')
+parser.add_argument('--EF', action='store', dest='EF', type=str,default='img_eva',help='val/train/test')
+parser.add_argument('--DATA_DIR', action='store', dest='DATA_DIR', type=str,
+                    default= 'C:\\...'
+                    ,help='Folder Path for images')
+parser.add_argument('--SET_TYPE', action='store', dest='SET_TYPE', type=str,
+                    default='D:\\...',
+                    help= 'Folder path for clinical baseline data')
+args = parser.parse_args()
+
+def load_weights(model, weight_path):
+    pretrained_weights = torch.load(weight_path, map_location=torch.device('cpu'))
+    model_weights = model.state_dict()
+
+    load_weights = {k: v for k, v in pretrained_weights.items() if k in model_weights}
+
+    model_weights.update(load_weights)
+
+    model.load_state_dict(model_weights)
+    print("Loading SELECTION...")
+    return model
+
+def set_random_seed(seed=0, deterministic=True):
+    """Set random seed.
+
+    Args:
+        seed (int): Seed to be used.
+        deterministic (bool): Whether to set the deterministic option for
+            CUDNN backend, i.e., set `torch.backends.cudnn.deterministic`
+            to True and `torch.backends.cudnn.benchmark` to False.
+            Default: False.
+    """
+    random.seed(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    if deterministic:
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+class Data(Dataset):
+    def __init__(self, set_type, img_dir,PathCat,transform=None, target_transform=None):
+        dict_path = set_type+'.pkl'
+        f = open(dict_path, 'rb')
+        # self.mm_data = pickle.load(f)
+        self.mm_data = pd.read_pickle(f)
+        f.close()
+        art_img_dir = os.path.join(img_dir,'arterial_SYSUCC_HAIC',PathCat)
+        port_img_dir = os.path.join(img_dir, 'portal_SYSUCC_HAIC',PathCat)
+        art_dirlist = os.listdir(art_img_dir)[0:]
+        self.art_img_dir = art_img_dir
+        self.port_img_dir = port_img_dir
+        self.idx_list = art_dirlist
+        self.art_dirlist = art_dirlist
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __len__(self):
+
+        return len(self.idx_list)
+        # return len(self.X_data)
+
+    def __getitem__(self, idx):
+        k = int(self.idx_list[idx].replace('.npy',''))
+
+        art_img_path = os.path.join(self.art_img_dir, str(k)) + '.npy'
+        port_img_path = os.path.join(self.port_img_dir, str(k)) + '.npy'
+
+        X1 = np.squeeze(np.load(art_img_path)).astype('float')
+        X_m_1 = np.expand_dims(X1, axis=-1)
+        art_img = np.concatenate([X_m_1, X_m_1, X_m_1], axis=-1)
+
+        X2 = np.squeeze(np.load(port_img_path))
+        X_m_2 = np.expand_dims(X2, axis=-1).astype('float')
+        port_img = np.concatenate([X_m_2, X_m_2, X_m_2], axis=-1)
+
+
+        label = self.mm_data[k]['label']    ## OOR
+        label2 = self.mm_data[k]['label2']      # 1Y-OS status
+        label3 = self.mm_data[k]['label3']*30       # OS time
+        label = np.array([label, label2,label3]).astype(float)
+
+        if self.transform:
+            art_img = self.transform(art_img)
+            port_img = self.transform(port_img)
+        if self.target_transform:
+            label = self.target_transform(label)
+
+        demo = torch.as_tensor(np.array([self.mm_data[k]['年龄'],self.mm_data[k]['性别'],self.mm_data[k]['ECOG (0或1)'],
+                                         self.mm_data[k]['合并症（糖尿病/高血压）'],self.mm_data[k]['肝炎 (0无 1乙肝 2丙肝）'],
+                                         self.mm_data[k]['腹水']])).float()
+        IV = torch.as_tensor(np.array([self.mm_data[k]['AFP'], self.mm_data[k]['数量'], self.mm_data[k]['最大肿瘤直径'],
+                      self.mm_data[k]['BCLC分期 '], self.mm_data[k]['肝外转移情况（0无  1有）'],
+                      self.mm_data[k]['门脉癌栓 ']])).float()
+        lab = torch.as_tensor(np.array([self.mm_data[k]['ALB'],self.mm_data[k]['ALT'],self.mm_data[k]['AST'],
+                                         self.mm_data[k]['TBIL'],self.mm_data[k]['PT'],self.mm_data[k]['INR'],
+                                         self.mm_data[k]['PLT'],self.mm_data[k]['child-pugh (5 or 6 or 7)']])).float()
+
+        return art_img.to(torch.float32),port_img.to(torch.float32), label, demo, lab , IV,k
+
+def test(args,load_model=None):
+    torch.manual_seed(args.seed)
+    num_classes = args.CLS
+    EXP = args.EXP
+    EXP_folder = args.EF
+    config = CONFIGS["SELECTION"] # Get default selection configuration from the code.
+
+    model = SELECTION(config, 224, zero_head=True, num_classes=num_classes)
+    model = torch.nn.DataParallel(model)
+    selection = load_weights(model, 'final_model\\'+EXP+'.pth')
+
+    img_dir = os.path.join(args.DATA_DIR,'Model_test')
+
+    ID= []
+    score = []
+    data_transforms = {
+        'test': transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.Resize(256),
+            transforms.CenterCrop(224),
+        ]),
+    }
+
+    test_data = Data(args.SET_TYPE, img_dir,EXP_folder, transform=data_transforms['test'])
+    testloader = DataLoader(test_data, batch_size=1, shuffle=False, num_workers=0, pin_memory=True)
+
+
+    # optimizer_selection = torch.optim.AdamW(selection.parameters(), lr=args.lr, weight_decay=0.01)
+    # selection, optimizer_selection = amp.initialize(selection.cuda(), optimizer_selection, opt_level="O1")
+
+    selection = torch.nn.DataParallel(selection)
+    # ----- Test ------
+    print('--------Start testing-------')
+    selection.eval()
+    with torch.no_grad():
+        for data in tqdm(testloader):
+            art_imgs,port_imgs, labels, demo, lab, IV,k = data
+            demo = demo.view(demo.shape[0],1,-1).cuda(non_blocking=True)
+            lab = lab.view(lab.shape[0],1,-1).cuda(non_blocking=True).float()
+            IV = IV.view(IV.shape[0],1,-1).cuda(non_blocking=True).float()
+            art_imgs = art_imgs.cuda(non_blocking=True)
+            port_imgs = port_imgs.cuda(non_blocking=True)
+            labels = labels.cuda(non_blocking=True)
+
+            preds = selection(art_imgs,port_imgs, lab, demo,IV)[0]
+
+
+
+def main():
+    set_random_seed(args.seed)
+    # test(args)
+if __name__ == '__main__':
+    main()
